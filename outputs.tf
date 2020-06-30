879ad0c3634178e8ac5f3c3397ccaa636cd43ccb
Shahzad A. Chaudhry <shahzad_chaudhry@hotmail.com>
2020-06-30 23:52:36 +0100

    Create security groups
    - ALB SG is created that accepts http:80 traffic from entire world
    - HTTP SG is created that accepts http:80 traffic from the public subnet
    
    Fixes #6

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vpc_id" {
-  value       = ""
+  value       = module.vpc.vpc_id
   description = "VPC ID"
 }
---
+++ security.tf
@@ -0,0 +1,27 @@
+# https://registry.terraform.io/modules/terraform-aws-modules/security-group
+
+module "alb_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "alb"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp"]
+  # there is two private subnet where ASG will be deployed to
+  egress_cidr_blocks  = tolist(module.vpc.private_subnets_cidr_blocks) 
+}
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  # there is only one public subnet where http traffic will come from
+  ingress_cidr_blocks = tolist(module.vpc.public_subnets_cidr_blocks) 
+  ingress_rules       = ["http-80-tcp"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
